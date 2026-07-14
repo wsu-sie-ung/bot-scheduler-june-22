f4d11f49c025e08cd3d95245c2fa2dd8aa45ed34
wsu-sie-ung <wsu.sie.ung@chiieftech.com.my>
2026-07-14 17:50:09 +0800

    dada

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -25,6 +25,19 @@ module.exports = {
                 NODE_ENV: "staging",
                 PORT: 3001,
             },
+        },
+        {
+            name: "worker-development",
+            script: "./dist/index.js",
+            instances: "1",
+            exec_mode: "cluster",
+            log_date_format: "YYYY-MM-DD HH:mm:ss:SSS Z",
+            error_file: "./logs/pm2-error-development.log",
+            out_file: "./logs/pm2-out-development.log",
+            env: {
+                NODE_ENV: "development",
+                PORT: 3002,
+            },
         }
     ]
 }
